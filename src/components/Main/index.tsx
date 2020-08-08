69de2430c60394065d56f2a4291748ee23b6af6c
unknown <adailsonacj@live.com>
2020-08-08 01:00:09 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,20 @@
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, NextJs, e Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Imagem de rapaz sentado na cadeira programando"
+    />
+  </S.Wrapper>
+)
+export default Main
---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import GlobalStyles from 'styles/global'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="description" content="A simple boilerplate" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
